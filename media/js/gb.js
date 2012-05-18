95f1d714398f6dd43162ba17040087ec41f23ee1
tom <tom>
2012-05-18 07:13:33 +0000

    Javascript von Mootools nach jQuery umgestellt.

--- media/js/gb.js
+++ media/js/gb.js
@@ -1,4 +1,135 @@
-window.addEvent('domready', function() {
+
+jQuery(function($){
+
+  function setSizes() {
+    // initialize
+    var winW = window.innerWidth,
+        winH = window.innerHeight,
+        is_landscape = winW > winH;
+
+    
+    // procedure for horizontal aspect ratio (landscape mode)
+    if (is_landscape) {
+      
+      // set navigation font sizes and distances
+      var contentWidth = $("#navigation .content").width(),
+          motherWidth = $("#mother").width(),
+          motherHeight = $("#mother").height(),
+          arrowSize = contentWidth / 17.3,
+          headlineFontSize = contentWidth / 8.8 + "px";
+
+      $(".navigation.headline").css({
+        "font-size": headlineFontSize,
+        "margin-left": arrowSize * 1.5 + "px"
+      });
+      
+      $("#foldout-btn").css({
+        "width": arrowSize + "px",
+        "height": arrowSize + "px",
+        "margin-top": (contentWidth / 55) - 6.5 + "px"
+      });
+
+      $("portfolio-list").css("margin-left", arrowSize * 1.5 + "px");
+
+      // set mother div dimensions
+      $("#mother").css({"width": "1014px", "height": "594px"});
+      if (winW > 1020 && winH > 600) {
+        $("#mother").css({"width": winW - 6 + "px", "height": winH - 6 + "px"});
+      } else if (winW < 1020 && winH > 600) {
+        $("#mother").css({"width": "1014px", "height": winH - 21 + "px"});
+      } else if (win_w > 1020 && win_h < 600) {
+        $("#mother").css({"width": winW - 21 + "px", "height": "594px"});
+      }
+
+      // set content boxes portfolio, teaching, faq, contact
+      $("#portfolio").css({
+        "width": motherWidth * 0.43 - 3 + "px",
+        "height": motherHeight + "px",
+        "left": motherWidth + "px"
+      });
+      $("#teaching").css({
+        "width": motherWidth * 0.4 + "px",
+        "height": motherHeight * 0.35 + "px",
+        "left": motherWidth * 0.6 + "px",
+        "top": motherWidth * 0.65 + "px"
+      });
+      $("#contact").css({
+        "width": motherWidth * 0.2 - 3 + "px",
+        "height": motherHeight * 0.65 - 6 + "px",
+        "left": motherWidth * 0.6 + "px"
+      });
+      $("#faq").css({
+        "width": motherWidth * 0.2 - 3 + "px",
+        "height": motherHeight * 0.65 - 6 + "px",
+        "left": motherWidth * 0.8 + "px",
+        "top": motherWidth * 0.65 + "px"
+      });
+      $("#faq #map").css("height", motherHeight * 0.65 * 0.4 + "px");
+
+    } else { 
+      // procedure for vertical aspect ratio (portrait mode)
+    }
+    
+    // set font-sizes in content boxes (both landscape and portrait mode)
+    setTimeout(function() {
+      // measure area of each content box
+      var teachingArea = $('#teaching').width() * $('#teaching').height(),
+          contactArea = $('#contact').width() * $('#contact').height(),
+          faqArea = $('#faq').width() * $('#faq').height(),
+          portfolioWidth = $("#portfolio").width();
+      
+      $("#portfolio .text").css({
+        "font-size": portfolioWidth / 23 + "px",
+        "line-height": portfolioWidth / 16.5 + "px"
+      });
+      
+      $("#teaching .content").css({
+        "font-size": teachingArea / 7600 + "px",
+        "line-height": teachingArea / 5100 + "px"
+      });
+      
+      if (contactArea > 250000) {
+        $("#contact .content").css({
+          "font-size": contactArea / 12000 + "px",
+          "line-height": contactArea / 7500 + "px"
+        });
+      } else {
+        $("#contact .content").css({
+          "font-size": "11px",
+          "line-height": "1.6em"
+        });
+      }
+      
+      if (faqArea > 250000) {
+        $("#faq .content").css({
+          "font-size": faqArea / 12000 + "px",
+          "line-height": faqArea / 7500 + "px"
+        });
+      } else {
+        $("#faq .content").css({
+          "font-size": "11px",
+          "line-height": "1.6em"
+        });
+      }
+    }, 500); 
+  }
+
+  $(window).resize(function() {
+    var oldWidth = window.innerWidth,
+        oldHeight = window.innerHeight;
+    setTimeout(function() {
+      if (oldWidth == window.innerWidth && oldHeight == window.innerHeight) {
+        set_sizes();
+      }
+    }, 250);
+  });
+
+  setSizes();
+});
+
+});
+
+/*window.addEvent('domready', function() {
   
   set_sizes();
   update_display();
@@ -238,4 +369,4 @@ function set_sizes() {
       });
     }
   }, 500); 
-}
+}*/
